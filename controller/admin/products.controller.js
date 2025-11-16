91ff88a41c7360484ffdb9f98475a4600f99ea53
le Duc Thien <ducthien09092005@gmail.com>
2025-11-16 15:01:57 +0700

    Lấy data in ra ngoài giao diện bên Admin

--- controller/admin/products.controller.js
+++ controller/admin/products.controller.js
@@ -1,7 +1,13 @@
 const Product = require("../../model/product.model");
 
 module.exports.index = async (req, res)=>{
+    const find = {
+        deleted: false,
+    }
+    const products = await Product.find(find);
+    console.log(products);
     res.render("admin/page/products/index", {
-        titlePage: "Danh sách sản phẩm"
+        titlePage: "Danh sách sản phẩm",
+        products: products
     })
 }

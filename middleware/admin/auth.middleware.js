6588d84748fa05361547a78ba403140dca08221a
le Duc Thien <ducthien09092005@gmail.com>
2026-01-04 13:41:56 +0700

    Làm chức năng Authorization

--- middleware/admin/auth.middleware.js
+++ middleware/admin/auth.middleware.js
@@ -1,5 +1,6 @@
 const prefixAdmin = require("../../config/system");
 const Account = require("../../model/account.model");
+const Role = require("../../model/role.model");
 
 module.exports.requireAuth = async (req, res, next)=>{
     if(!req.cookies.token){
@@ -8,8 +9,13 @@ module.exports.requireAuth = async (req, res, next)=>{
     }else{
         const record = await Account.findOne({
             token: req.cookies.token
-        });
+        }).select("-password");
         if(record){
+            const role = await Role.findOne({
+                _id: record.role_id
+            }).select("title permission");
+            res.locals.role = role;
+            res.locals.user = record;
             next();
         }else{
             res.redirect(`${prefixAdmin.prefixAdmin}/auth/login`);

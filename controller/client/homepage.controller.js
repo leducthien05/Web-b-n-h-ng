c4ded2b269f37b99f9ed3fbc59500215f991da76
le Duc Thien <ducthien09092005@gmail.com>
2026-01-12 15:09:42 +0700

    Làm danh sách sản phẩm bên client

--- controller/client/homepage.controller.js
+++ controller/client/homepage.controller.js
@@ -1,9 +1,18 @@
 const Product = require("../../model/product.model");
 const Category = require("../../model/category-product.model");
 
+const newPriceHelper = require("../../helper/newPrice.helper");
 
 module.exports.homepage = async (req, res)=>{
+    const productFeatured = await Product.find({
+        deleted:false,
+        status: "active",
+        featured: "1"
+    });
+    const newProduct = newPriceHelper.newPriceArray(productFeatured);
+
     res.render("client/page/home/index", {
-        titlePage: "Trang chủ"
+        titlePage: "Trang chủ",
+        productFeatured: newProduct
     });
 }
